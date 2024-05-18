f79884a7205fb888351084474227894d1d88398c
Mateusz Grudziński <77492508+MateuszGrudzinski@users.noreply.github.com>
2024-05-18 21:10:32 +0200

    Get_data.py improvments| Addition of target data

--- Get_data.py
+++ Get_data.py
@@ -18,22 +18,42 @@ def Get_n_Year_data(N_of_years,Stat_type,Player_type,User_name = ''):
 def Save_Data_to_Pickle(leagueleader,names,dir_path):
     i = 0
     for df in leagueleader:
-        print(i)
-        df.to_pickle(dir_path + names[i])
+        df.to_pickle(dir_path + names[i] + ".pkl")
         i += 1
 
 def Read_Data_from_Pickles(names,dir_path):
     leagueladers = []
     for name in names:
-            df = pd.read_pickle(dir_path + name)
+            df = pd.read_pickle(dir_path + name + ".pkl")
             leagueladers.append(df)
     return leagueladers
 
-Data, Names = Get_n_Year_data(30,"Regular Season",'RS')
-Save_Data_to_Pickle(Data,Names,"./Data/")
-Data, Names = Get_n_Year_data(30,"Regular Season",'Rookies','_Rookies')
-Save_Data_to_Pickle(Data,Names,"./Data/")
-Data_loaded = Read_Data_from_Pickles(Names,"./Data/")
-print(Data_loaded[0])
+def Load_data_form_CSV(names,dir_path):
+    data = []
+    for name in names:
+            df = pd.read_csv(dir_path + name + ".csv")
+            data.append(df)
+    return data
+
+def Combine_Data_and_target(Data,Target,season):
+    result = Data.copy()
+    result["Target"] = 0
+    target_class = 1
+    for i in range (25):
+        result.loc[result["PLAYER"] == Target[season].iloc[i],'Target'] = target_class
+        if i % 5 == 4:
+            target_class += 1
+    return result
+
+if __name__ == "__main__":
+    Data, Names = Get_n_Year_data(30,"Regular Season",'RS')
+    Save_Data_to_Pickle(Data,Names,"./Data/")
+    #Data, Names = Get_n_Year_data(30,"Regular Season",'Rookies','_Rookies')
+    #Save_Data_to_Pickle(Data,Names,"./Data/")
+    Data_loaded = Read_Data_from_Pickles(Names,"./Data/")
+    target = Load_data_form_CSV(["target"],"./Target/")
+    combined = Combine_Data_and_target(Data[0],target[0],"2022-23")
+    combined_tr = combined[['PLAYER','Target']].copy()
+    print(combined[['PLAYER','Target']])
 
 
---
+++ Target/train.py
@@ -0,0 +1,12 @@
+import pickle
+import pandas as pd
+import numpy as np
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, ConfusionMatrixDisplay
+from sklearn.model_selection import RandomizedSearchCV, train_test_split
+from scipy.stats import randint
+
+if __name__ == "__main__":
+    print("Goodbye World")
+
